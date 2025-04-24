6b0edc11bbdbe48b9b59d3e0644827d4a99c2634
harshita9708 <harshitahosamani04@gmail.com>
2025-04-24 11:33:54 +0530

    save java

---
+++ Demo1.java
@@ -0,0 +1,13 @@
+
+public class Demo1{
+   
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        System.out.println("Addition result: "+(a+b));
+        System.out.println("Addition result: "+(a-b));
+        System.out.println("Addition result: "+(a*b)); 
+        System.out.println("Addition result: "+(a/b));
+    }
+}
