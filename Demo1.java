1dfb5f26b6bd35a4002b297e2fdeb316860af115
harshita9708 <harshitahosamani04@gmail.com>
2025-04-24 11:38:10 +0530

    save java

--- Demo1.java
+++ Demo1.java
@@ -6,8 +6,10 @@ public class Demo1{
         int a=10;
         int b=20;
         System.out.println("Addition result: "+(a+b));
-        System.out.println("Addition result: "+(a-b));
-        System.out.println("Addition result: "+(a*b)); 
-        System.out.println("Addition result: "+(a/b));
+        System.out.println("Substraction result: "+(a-b));
+        System.out.println("Multipliaction result: "+(a*b)); 
+        System.out.println("Division result: "+(a/b));
+        System.out.println("Module result: "+(a%b));
+
     }
 }
