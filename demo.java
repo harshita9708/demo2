1b4f37b786748f3e3e21583f9ee0c8a3030a1962
harshita9708 <harshitahosamani04@gmail.com>
2025-04-24 11:12:20 +0530

    save file

--- demo.java
+++ demo.java
@@ -7,6 +7,7 @@ public class demo{
         int b=20;
         System.out.println("Addition result: "+(a+b));
         System.out.println("Addition result: "+(a-b));
- 
+        System.out.println("Addition result: "+(a*b)); 
+        System.out.println("Addition result: "+(a/b));
     }
 }
